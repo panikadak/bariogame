196086a00fa5a3a9a67413fd2a75247b7064b22a
basebasebase <panikadak@gmail.com>
2024-09-10 03:26:28 +0300

    launch

---
+++ bario-knight/assets/maps/KnightPlatformer-fca2e35d970df7dfb5f239833c2ceb7b.tsx
@@ -0,0 +1,174 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="KnightPlatformer" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="256" columns="16">
+ <image source="world_tileset_pad.png" width="288" height="288"/>
+ <tile id="0">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="Goal" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="BType" value="BLOCK"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="BType" value="PLAYER"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="BType" value="FOE"/>
+  </properties>
+ </tile>
+</tileset>
